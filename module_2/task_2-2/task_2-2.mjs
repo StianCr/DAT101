80084ba0f36103c3890f17fdf6f698d7eadc02c3
stiancr <stiancr@uia.no>
2024-10-09 14:39:49 +0200

    Oppgave 2.2

--- module_2/task_2-2/task_2-2.mjs
+++ module_2/task_2-2/task_2-2.mjs
@@ -5,7 +5,7 @@ initPrintOut(document.getElementById("txtOut"));
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
 const x = 2 + 3 * (2-4) * 6;
-printOut ("2 + 3(2 - 4) 6 =" + x.tostring);
+printOut ("2 + 3(2 - 4) 6 =" + x.toString());
 printOut(newLine);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
@@ -19,7 +19,7 @@ printOut("--- Part 2 -----------------------------------------------------------
 const metres = 25.34;
 const answer = (metres * millInMeters) / millInInch;
 
-printOut("answer = " + answer.toString());
+printOut("answer = " + answer.toFixed());
 
 printOut(newLine);
 
@@ -43,13 +43,24 @@ printOut("--- Part 4 -----------------------------------------------------------
 /* Convert 6,322.52 minutes to days, hours, minutes, and seconds.
 */
 const numberOfMinutes = 6322.52;
-let part4_Calc = numberOfMinutes / (60 * 24);
+let part4_Calc = numberOfMinutes / (60*24);
+const part4_days = Math.floor(part4_Calc);
+printOut("Days = " + part4_days)
+
+part4_Calc = part4_Calc - part4_days
+part4_Calc = part4_Calc * hoursInDay;
 const part4_Hours = Math.floor(part4_Calc);
-printOut ("Hours = " + part4_Hours);
+printOut("hours =" + part4_Hours);
 
 part4_Calc = part4_Calc - part4_Hours;
-printOut("part4_Calc = " + part4_Calc);
+part4_Calc = part4_Calc * minutesInHour;
+const part4_Minutes = Math.floor(part4_Calc);
+printOut("Minutes = " + part4_Minutes);
 
+part4_Calc = part4_Calc - part4_Minutes
+part4_Calc = part4_Calc * secondsInMinutes;
+const part4_Seconds = Math.floor(part4_Calc);
+printOut("Seconds = " + part4_Seconds);
 printOut(newLine);
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
@@ -82,32 +93,98 @@ printOut(text.indexOf("earth").toString());
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("5 er større enn 3 (5 > 3)"); // true
-printOut("7 er like stor som 7 ( 7 == 7");
+let a = "a";
+let b = "b";
+printOut("is 5 greater than 3"); // true
+printOut('True');
+printOut("is 7 greater or equal to 7");
+printOut("True");
+printOut("is 'a' greater than 'b'");
+if (a > b) {
+  printOut("true");
+} else {
+  printOut("false");
+}
+
+printOut("is '1' less than 'a'?")
+if (1>a){
+  printOut("true");
+}   else{
+  printOut("false");
+}
+printOut("Is '2500' less than 'abcd'?")
+
+let abcd = "abcd";
+
+if (2500 < abcd) {
+    printOut("true");
+}   else{
+    printOut("false");
+}
+
+printOut("'arne' is not equal to 'thomas'.")
+
+let arne = "arne";
+let thomas = "thomas";
+
+if (arne = thomas) {
+    printOut("true");
+}   else{
+    printOut("false");
+}
+
+printOut("(2 equals 5) is this statement true?");
+printOut("false");
+printOut("'abcd' is greater than 'bcd'?");
+
+let bcd = "bcd";
+
+if (abcd > bcd) {
+    printOut("true");
+}   else{
+    printOut("false");
+}
 
 
-printOut("Replace this with you answer!");
 printOut(newLine);
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-let text1 = "254";
-printOut(text1);
+let numberAsText = "254";
+let number_254 = parseInt(numberAsText, 10);
+printOut(number_254.toString());
+
+let _57_23 = "57.23";
+let number_57_23 = parseFloat(_57_23, 10);
+printOut(number_57_23.toString());
+
+let _25_kroner = "25 kroner";
+let number_25_kroner = parseInt(_25_kroner,10);
+printOut(number_25_kroner.toString());
 
 printOut(newLine);
 
 printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-let r = Math.floor(math.random()) * 360 + 1;
-printOut(r);
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let r = getRandomInt(1, 360);
+printOut(r.toString());
+
+
 printOut(newLine);
 
 
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-let totaldays = 131;
-let weeks = math.floor(totaldays / 7);
-let days = totaldays % 7;
-printOut(`131 days is equal to ${weeks} weeks and ${days} days.`);
+let totalDays = 131;
+
+let weeks = Math.ceil(totalDays / 7);
+let days = totalDays % 7;
+
+printOut(`${totalDays} days is equal to ${weeks} weeks and ${days} days.`);
+
 
 printOut(newLine);
